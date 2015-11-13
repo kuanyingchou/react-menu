133695fd04b944ebd38ee9a765fca30080149090
KUAN-YING CHOU <kuanyingchou@gmail.com>
2015-11-12 16:47:56 -0800

    fix babel

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  entry: './app.js',
+  output: { filename: 'bundle.js' },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+};
