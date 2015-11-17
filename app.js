d412f24678f0b5c0567ed5e04e579d1357d362b4
KUAN-YING CHOU <kuanyingchou@gmail.com>
2015-11-17 15:39:19 -0800

    add top and left sub-menu

--- app.js
+++ app.js
@@ -3,44 +3,57 @@ import React from 'react';
 import Menu from './menu';
 import { Direction } from './constants';
 
-let m3 = {
-  position: 'down',
+let thm = {
+  position: 'top',
   layout: 'horizontal',
-  items: [ {name: 'Good'}, {name: 'Let\'s Go'} ],
+  items: [ {name: '1'}, {name: '2'} ],
+}
+
+let lhm = {
+  position: 'left',
+  layout: 'horizontal',
+  items: [ {name: '1'}, {name: '2'} ],
 };
 
-let m2 = {
-  layout: 'vertical',
+let dhm = {
+  position: 'down',
+  layout: 'horizontal',
+  items: [ {name: '1'}, {name: '2'} ],
+};
+
+let rhm = {
+  layout: 'horizontal',
   position: 'right',
   items: [
     { name: 'Well', url: 't1.html' },
-    { name: 'Site', submenu: m3 },
+    { name: 'Site', submenu: dhm },
   ],
 };
 
-let m1 = {
-  position: 'right',
-  items: [
-    { name: 'iPad Pro', submenu: m2},
-    { name: 'iPhone', url: 'a2.html'},
-    { name: 'Macbook Pro', submenu: m3},
-  ],
-};
+// let m1 = {
+//   position: 'right',
+//   items: [
+//     { name: 'iPad Pro', submenu: m2},
+//     { name: 'iPhone', url: 'a2.html'},
+//     { name: 'Macbook Pro', submenu: m3},
+//   ],
+// };
 
 let menu = {
-  layout: 'vertical',
+  layout: 'horizontal',
   // layout: 'horizontal',
   items: [
-    { name: 'Home', url:'home.html', },
-    { name: 'Articles', url:'articles.html' },
-    { name: 'About', submenu: m1},
+    { name: 'Right', url:'home.html', submenu: rhm},
+    { name: 'Left', url:'home.html', submenu: lhm},
+    { name: 'Top', url:'articles.html', submenu: thm },
+    { name: 'Down', submenu: dhm },
   ],
 };
 
 render(
   // <Menu config = { m1 } />,
   // <Menu config = { m3 } />,
-  <div>
+  <div style={{ textAlign: 'center', }}>
     <p>
       This is the header.
     </p>
--- menu.js
+++ menu.js
@@ -2,25 +2,68 @@ import React, { Component } from 'react';
 import MenuItem from './menuitem';
 
 class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { width: 0, height: 0 };
+  }
 
-  getStyle(level, position, parentWidth, parentHeight) {
-    return {
+  componentDidMount() {
+    let dom = this.refs.menu;
+    this.setState({width: dom.offsetWidth, height: dom.offsetHeight});
+  }
+
+  getPositionStyle(level, position, width, height, parentWidth, parentHeight) {
+    if(level == 0) return {};
+    switch(position) {
+    case 'top':
+      return {
+        left: 0,
+        top: -height,
+      };
+    case 'down':
+      return {
+        left: 0,
+        top: parentHeight,
+      };
+    case 'left':
+      return {
+        left: -width,
+        top: 0,
+      };
+    case 'right':
+      return {
+        left: parentWidth,
+        top: 0,
+      };
+    default:
+      console.log(`cannot recognize position: ${position}`);
+    }
+  }
+
+  getStyle(level, position, width, height, parentWidth, parentHeight) {
+    let style = {
       position: level==0?'relative':'absolute',
       display: 'inline-block',
       //display: position=='right'?'inline-block':'block',
       boxSizing: 'border-box',
       verticalAlign: 'top',
-      left: level==0 || position=='down'?0:parentWidth-1,
-      top: position=='down'?parentHeight-1:0,
+      left: 0,
+      top: 0,
+      // left: level==0 || position=='down'?0:parentWidth-1,
+      // top: position=='down'?parentHeight-1:0,
+      zIndex: level,
 
       // users should be able to change these
       borderStyle: 'solid',
       borderColor: 'grey',
       borderWidth: '1px',
       background: 'lightgrey',
-      boxShadow: '3px 3px 5px lightgrey',
+      boxShadow: '3px 3px 5px rgba(0, 0, 0, 0.5)',
       borderRadius: '2px',
     };
+    return Object.assign(style,
+      this.getPositionStyle(level, position, width,
+          height, parentWidth, parentHeight));
   }
 
   render() {
@@ -45,10 +88,11 @@ class Menu extends Component {
         />
       )
     );
+    let { width, height } = this.state;
     let style = this.getStyle(
-        level, position, parentWidth, parentHeight);
+        level, position, width, height, parentWidth, parentHeight);
     return (
-      <div style={ style } >
+      <div style={ style } ref="menu" >
         { menuItems }
       </div>
     );
